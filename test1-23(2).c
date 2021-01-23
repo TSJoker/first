a935b63507684f48e8b80c6def0ebd5f63065689
Tony <77772783+TSJoker@users.noreply.github.com>
2021-01-23 11:21:44 +0800

    Add files via upload

---
+++ test1-23(2).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char arr1[] = "joker";
+	char arr2[] = { 'j','o','k','e','r' };
+	printf("%d\n", strlen(arr1));//²â×Ö·û´®³¤¶È
+	printf("%d\n", strlen(arr2));
+	return 0;
+}
