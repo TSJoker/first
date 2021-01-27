edaf47373c7d2b2995256cbdf3e2be0e7f6c8f54
Tony <77772783+TSJoker@users.noreply.github.com>
2021-01-27 15:27:25 +0800

    Add files via upload

---
+++ test1-27(3).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct Person
+{
+	char name[20];//定义结构体姓名
+	long  stnum;//定义结构体学号
+	char major[20];//专业
+	char class[20];//班级
+	char grade[20];//年级
+
+};
+int main()
+{
+	struct Person P1 = { "李颖",1862020222,"汉语国际教育","二班","18级" };//创造一个该类型的结构体变量
+	struct Person* D = &P1;
+	printf("姓名:%s\n", D->name);//P.name表示结构体变量.成员
+	printf("学号:%d\n", D->stnum);
+	printf("专业:%s\n", D->major);
+	printf("班级:%s\n", D->class);
+	printf("年级:%s\n", D->grade);
+	return 0;
+}
