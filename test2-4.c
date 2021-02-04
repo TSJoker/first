bb85017cf68d42c6035c9aeb7fdc1e12dc32dde4
Tony <77772783+TSJoker@users.noreply.github.com>
2021-02-04 16:52:21 +0800

    Update test2-4.c

--- test2-4.c
+++ test2-4.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int ch = 0;
-	while ((ch = getchar()) != EOF)//EOF->end of file
-		putchar(ch);
+	while ((ch = getchar()) != EOF)//EOF->end of file,即Ctrl+c键
+		putchar(ch);//输出字符->putchar,输入字符->getchar
 	return 0;
 }
